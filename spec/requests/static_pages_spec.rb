d70fda17253b79b8372a214cc31977ba4917735b
Mike Kowalsky <innoender@gmail.com>
2013-08-29 14:07:17 -0500

    Add contact and refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do 
 
+	let (:base_title) { "Ruby on Rails Tutorial" }
+
 
 	describe "Home page" do 
 		it "should have the content 'Sample App3'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
 
 		it "should have the tite 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -35,7 +37,19 @@ describe "Static pages" do
 
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial | About")
+			expect(page).to have_title("#{base_title} | About")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the contect 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content("Contact Us")
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 end
