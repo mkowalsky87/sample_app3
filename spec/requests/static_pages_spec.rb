c85abe754fd4b6710253a87b461956ae0f414819
Mike Kowalsky <innoender@gmail.com>
2013-08-29 13:47:22 -0500

    Finish Static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content("Sample App3")
 		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial | Home")
+		end
 	end
 
 	describe "Help page" do
@@ -15,12 +20,22 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content("Help")
 		end
+
+		it "should have the tite 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial | Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
-			visit 'static_pages/about'
+			visit '/static_pages/about'
 			expect(page).to have_content("About Us")
+		end
+
+		it "should have the title 'About'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial | About")
 		end
 	end
 end
